9657e35a40be4d24aecc07a5943656aab18f0211
Christine Frezek <cfrezek@yahoo.com>
2019-01-30 20:59:34 -0500

    converted replit ot github

---
+++ scripts.js
@@ -0,0 +1,85 @@
+var questionsArray = [
+    //Question 1
+    {
+        questionText: 'What city hosted the 2012 summer Olympic games?',
+        questionChoices: ['Rio de Janeiro', 'London', 'Sydney', 'Beijing'],
+        questionCorrectChoice: 1,
+        correctDetails: 'London'
+    },
+
+    //Question 2
+
+    {
+        questionText: 'Which US president does not have his face carved on Mount Rushmore?',
+        questionChoices: [' George Washington', 'Thomas Jefferson', 'Franklin Delano Roosevelt', 'Abraham Lincoln'],
+        questionCorrectChoice: 2,
+        correctDetails: ' Franklin Delano Roosevelt is not on Mount Rushmore. It is actually Theodore Roosevelt.'
+    },
+
+    //Question 3
+    {
+        questionText: ' As of fall 2016, approximately how many bison lived in Yellowstone National Park?',
+        questionChoices: ['1000', '3000', '5000', '7000'],
+        questionCorrectChoice: 2,
+        correctDetails: '5000’'
+    },
+
+    //Question 4
+    {
+        questionText: ' In what Italian city is the Trevi Fountain located?',
+        questionChoices: ['Florence', 'Milan', 'Rome', 'Venice'],
+        questionCorrectChoice: 2,
+        correctDetails: 'Rome'
+    },
+
+    //Question 5
+    {
+        questionText: 'How long is the Great Wall of China?',
+        questionChoices: ['10,230 miles', '11,770 miles', '13,170 miles', '14,540 miles'],
+        questionCorrectChoice: 2,
+        correctDetails: '13,170 miles'
+    },
+
+
+    //Question 6
+    {
+        questionText: 'What Scottish lake is home to a legendary monster?',
+        questionChoices: ['Awe', 'Ewe', 'Lomond', 'Ness'],
+        questionCorrectChoice: 3,
+        correctDetails: 'Ness'
+    },
+
+    //Question 7
+    {
+        questionText: 'What civilization is connected to the citadel of Macchu Picchu?',
+        questionChoices: ['Aztec', 'Mayan', 'Naxca', 'Inca'],
+        questionCorrectChoice: 3,
+        correctDetails: 'Inca'
+    },
+
+    //Question 8
+    {
+        questionText: 'How many islands of the Republic of Fiji are inhabited?',
+        questionChoices: ['97', '110', '332', '478'],
+        questionCorrectChoice: 1,
+        correctDetails: '110'
+    },
+
+    //Question 9
+    {
+        questionText: 'What is the Melbourne Cup?',
+        questionChoices: ['A popular bar in Australia', 'A horse race', 'A piece of Aboriginal art', 'A tribute to the monarchy'],
+        questionCorrectChoice: 1,
+        correctDetails: 'A horse race'
+    },
+
+    //Question 10
+    {
+        questionText: 'How many national parks are there in the US?',
+        questionChoices: ['48', '58', '68', '78'],
+        questionCorrectChoice: 1,
+        correctDetails: '58'
+    },
+];
+
+console.log(questionsArray);
